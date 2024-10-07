10fb7b1f8396382e0f5f486971f1ab1fca917e7b
Karina Muñoz <karina.m.soto@gmail.com>
2024-10-07 14:02:31 -0300

    nuevo avance de entrega, pronto a subir

--- js/main.js
+++ js/main.js
@@ -2,65 +2,72 @@
 // La idea de este "programa" es que al escribir uno de los títulos preestablecidos, se retorne una breve "descripción" tipo muy resumida.
 
 
-  /*let book= prompt ("¿Qué libro quieres conocer?: It , El Instituto, El Visitante, La Sangre Manda , El Resplandor")
-     switch (book){
-      case "It":
-    alert ("El libro en que aparece Pennywise, el mal de un pueblo llamado Derry");
-    break;
-  case "El Instituto":
-    alert ("Donde aparecen niños con poderes, que son llevados a un lugar para ser utilizados como armas");
-    break;
-  case "El Visitante":
-    alert ("Ente malvado que toma imagen de una persona y se alimenta de humanos");
-    break;
-  case "La Sangre Manda":
-    alert ("Recompilación de cuentos cortos, además de un caso con Holly Gibney");
-    break;
-  case "El Resplandor":
-    alert ("Una familia va a un hotel a cuidarlo durante el invierno, pero el padre de familia se vuelve loco");
-    break;
-  default:
-    alert("Introdujiste una opción no válida")
-    break;
-}*/
-
-//--------------------------------------------
-
 //Array con títulos de Libros de Stephen King
-//const books = ["It", "El Instituto","El Visitante","La Sangre Manda", "El Resplandor"];
-//chooseABook
-//searchBook
 
-//ACÁ SE DECLARA LA FUNCIÓN
-
-// function searchBook(bookTittle)
-
-let books;
+let books = ["It", "El Instituto","El Visitante","La Sangre Manda", "El Resplandor"];
+let chooseABook;
 
 do{
-  books = prompt ("¿Qué libro quieres conocer?: A = It , B = El Instituto, C = El Visitante, D = La Sangre Manda , E = El Resplandor, F = Salir ");
- 
-  switch (books){
+  chooseABook=prompt("¿Qué libro quieres conocer?: A = It , B = El Instituto, C = El Visitante, D = La Sangre Manda , E = El Resplandor, F = Salir ");
+
+  switch(chooseABook){
     case "A":
+      if (books="A"){
       alert ("It = El libro en que aparece Pennywise, el mal de un pueblo llamado Derry");
-      break;
+      
+      }else {
+        console.log(books);
+      }
+     break;
+
     case "B":
+      if (books="B"){
       alert ("El Instituto = Donde aparecen niños con poderes, que son llevados a un lugar para ser utilizados como armas");
-      break;
+      
+    }else {
+      console.log(books);
+    }
+    break;
+
     case "C":
+      if (books="C"){
       alert ("El Visitante = Ente malvado que toma imagen de una persona y se alimenta de humanos");
-      break;
+      
+    }else {
+      console.log(books);
+    }
+    break;
+    
     case "D":
+      if (books="D"){
       alert ("La Sangre Manda = Recompilación de cuentos cortos, además de un caso con Holly Gibney");
-      break;
-  case "E":
-    alert ("El Resplandor Una familia va a un hotel a cuidarlo durante el invierno, pero el padre de familia se vuelve loco");
+      
+    }else {
+      console.log(books);
+    }
     break;
-  case "F":
-    alert ("Hasta Pronto!");
+
+    case "E":
+      if (books="E"){
+      alert ("El Resplandor Una familia va a un hotel a cuidarlo durante el invierno, pero el padre de familia se vuelve loco");
+      
+    }else {
+      console.log(books);
+    }
     break;
-  default:
+
+    case "F":
+      if (books="F"){
+      alert ("Hasta Pronto!");
+      
+    }else {
+      console.log(books);
+    }
+    break;
+
+    default:
     alert("Introdujiste una opción no válida")
     break
   }
-}while (books !== "F");
+
+} while (chooseABook !=="F");
